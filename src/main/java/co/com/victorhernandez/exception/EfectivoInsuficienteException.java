1f6e3500d9fd077855a92ee55cdcffb4b5c08c38
Victor Hernandez <victormanu968@gmail.com>
2022-08-03 09:13:00 -0500

    Metodos  implementados

---
+++ src/main/java/co/com/victorhernandez/exception/EfectivoInsuficienteException.java
@@ -0,0 +1,9 @@
+package co.com.victorhernandez.exception;
+
+public class EfectivoInsuficienteException extends Exception{
+
+    public EfectivoInsuficienteException(String message) {
+        super(message);
+    }
+    
+}
